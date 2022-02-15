682189163e6d6d60ac373d8e26d82448c42edf64
katfloyd95 <katarina.floyd1@gmail.com>
2022-02-15 16:44:59 +0100

    Print Hello Name Solution

--- 1-printHelloName.js
+++ 1-printHelloName.js
@@ -1,2 +1,18 @@
 // JAVASCRIPT CODING CHALLENGES
 
+/* 
+  Create a string to say hello in the format "Hello, {{name}}!"
+  Ex. printHelloName("Aaron"), returns "Hello, Aaron!"
+*/
+
+var printHelloName = function(firstName) {
+  return `Hello, ${firstName}!`;
+}
+
+printHelloName("Aaron");
+
+// Test block:
+const printHelloNameTests = [
+  {args: ["Aaron"], expected: "Hello, Aaron!"},
+  {args: [""], expected: "Hello, !"}
+]
